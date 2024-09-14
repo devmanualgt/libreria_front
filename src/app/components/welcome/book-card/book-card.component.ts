54b8f7e66c649d38ba1c12d1c2d0ae0ef647ff0e
mconcoba <mconcoba20@gmail.com>
2024-09-13 21:45:20 -0600

    feat(add): last config

--- src/app/components/welcome/book-card/book-card.component.ts
+++ src/app/components/welcome/book-card/book-card.component.ts
@@ -8,48 +8,64 @@ import CarouselComponent from '../../carousel/carousel.component';
 @Component({
   selector: 'app-book-card',
   standalone: true,
-  imports: [CommonModule,
-    RouterModule,
-    
-    ],
+  imports: [CommonModule, RouterModule],
   template: `
-
-
-
-  
-  <div class="card" 
-  style="
+    <div
+      class="card"
+      style="
     width: 18rem;
-    
-  ">
 
-    <div class="card-body">
-    <img [src]="book?.link_img" [alt]="book?.title" style="width: 100%; height: 50%">
-    <p class="card-title fs-3"><span class="fw-bolder">{{book?.title}}</span></p>
-    <p class="card-text"><span class="fw-bold">Autor:</span> {{book?.author}}</p>
-    <p class="card-text"><span class="fw-bold">Temas:</span> {{book?.topics}}</p>
-    <p class="card-text"><span class="fw-bold">Isbn:</span> {{book?.isbn}}</p>
-    <div class="d-flex justify-content-evenly gap-2">
-      <a [routerLink]="['/details-book',book?.id]" class="btn btn-primary" style="width: 100%;" >Editar</a>
-      <a [routerLink]="['/welcome']" class="btn btn-danger" style="width: 100%;" (click)="deleteBook(book?.id)">Eliminar</a>
+  "
+    >
+      <div class="card-body">
+        <img
+          [src]="book?.link_img"
+          [alt]="book?.title"
+          style="width: 100%; height: 400px"
+        />
+        <div>
+          <p
+            class="card-title fs-3"
+            style="height: 100px; display: flex; align-items: center; justify-content: center;"
+          >
+            <span class="fw-bolder">{{ book?.title }}</span>
+          </p>
+        </div>
+        <p class="card-text">
+          <span class="fw-bold">Autor:</span> {{ book?.author }}
+        </p>
+        <p class="card-text">
+          <span class="fw-bold">Temas:</span> {{ book?.topics }}
+        </p>
+        <p class="card-text">
+          <span class="fw-bold">Isbn:</span> {{ book?.isbn }}
+        </p>
+        <div class="d-flex justify-content-evenly gap-2">
+          <a
+            [routerLink]="['/details-book', book?.id]"
+            class="btn btn-primary"
+            style="width: 100%;"
+            >Editar</a
+          >
+          <a
+            [routerLink]="['/welcome']"
+            class="btn btn-danger"
+            style="width: 100%;"
+            (click)="deleteBook(book?.id)"
+            >Eliminar</a
+          >
+        </div>
+      </div>
     </div>
-    
-    </div>
-  </div>
-
-  
-  
-  `
+  `,
 })
 export class BookCardComponent {
-
-  constructor(private AuthService: AuthService, ){}
-
+  constructor(private AuthService: AuthService) {}
 
   // deleteBook(bookId: number){
 
   //   this.AuthService.deleteBook(bookId);
-  // } 
+  // }
 
   deleteBook(bookId?: number) {
     if (bookId !== undefined) {
@@ -58,8 +74,6 @@ export class BookCardComponent {
       console.error('El ID del libro es indefinido.');
     }
   }
-  
-  
+
   @Input() book?: Book;
-  
 }
--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -10,27 +10,26 @@ import { environment } from '../environments/environment';
 import { Book } from '../interfaces/all-books.interface';
 import { newBook } from '../interfaces/new-book';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient,
+  constructor(
+    private http: HttpClient,
     private router: Router,
-    private message: MessageService,
-
+    private message: MessageService
   ) {}
-   
+
   getToken() {
     return localStorage.getItem('accessToken');
   }
-  
-  setToken(token:string): void {
+
+  setToken(token: string): void {
     localStorage.setItem('accessToken', token);
   }
-  
+
   getCurrentUser(): any {
     const currentUserString = localStorage.getItem('currentUser');
     if (currentUserString) {
@@ -39,23 +38,18 @@ export class AuthService {
     return null;
   }
 
-  
-  auth(post: UserLogin ){
-    
-    return this.http.post<any>(`${this.apiUrl}auth/login`,post).subscribe({
-      next: (response)=> {
-        
+  auth(post: UserLogin) {
+    return this.http.post<any>(`${this.apiUrl}auth/login`, post).subscribe({
+      next: (response) => {
         this.setToken(response.records.accessToken);
         this.router.navigate(['/welcome']);
-        
       },
       error: (error) => {
         error;
         // alert(error.error.message);
         this.message.errorAlert(error.error.message);
-      }
-      
-    }); 
+      },
+    });
   }
 
   logout(): void {
@@ -63,30 +57,29 @@ export class AuthService {
     localStorage.removeItem('currentUser');
     this.router.navigate(['/login']);
   }
-  
-  
+
   // libros-----------------------------
-  
-  getAllBooks(): Observable<any>{
+
+  getAllBooks(): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `${this.getToken()}`,
+      Authorization: `${this.getToken()}`,
       // 'access_token': `Bearer ${this.getToken()}`,
       // 'accessToken': `${this.getToken()}`,
-      
     });
 
     return this.http.get<any>(`${this.apiUrl}books`, { headers });
   }
-  newBook(newBook: newBook ){
+  newBook(newBook: newBook) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `${this.getToken()}`
-      
+      Authorization: `${this.getToken()}`,
     });
-    
-    return this.http.post<any>(`${this.apiUrl}books`,newBook, {headers}).subscribe({
-      next: (response)=> {
+
+    return this.http
+      .post<any>(`${this.apiUrl}books`, newBook, { headers })
+      .subscribe({
+        next: (response) => {
           response;
           this.message.successAlert();
           this.router.navigate(['/welcome']);
@@ -94,68 +87,63 @@ export class AuthService {
         error: (error) => {
           error;
           this.message.errorAlert(error.error.message);
-
-        }
+        },
       });
-    
-    }
+  }
 
-    getBookByid(id:number): Observable<any>{
-      const headers = new HttpHeaders({
+  getBookByid(id: number): Observable<any> {
+    const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `${this.getToken()}`,
-      
+      Authorization: `${this.getToken()}`,
     });
     return this.http.get(`${this.apiUrl}books/${id}`, { headers });
- 
   }
 
   deleteBook(bookId: number) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `${this.getToken()}`
+      Authorization: `${this.getToken()}`,
     });
-  
-    return this.http.delete<any>(`${this.apiUrl}books/${bookId}`, { headers }).subscribe({
-      next: (response) => {
-        // this.message.deleterAlert();
-        this.message.successAlert(),
-        setTimeout(() => {
-          window.location.reload();
-        }, 2000);
-      },
-      error: (error) => {
-        this.message.errorAlert(error.error.message);
-      }
-    });
+
+    return this.http
+      .delete<any>(`${this.apiUrl}books/${bookId}`, { headers })
+      .subscribe({
+        next: (response) => {
+          // this.message.deleterAlert();
+          this.message.successAlert(),
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+        },
+        error: (error) => {
+          this.message.errorAlert(error.error.message);
+        },
+      });
   }
 
-  
   updateBook(bookId: string, updatedData: newBook) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `${this.getToken()}`
+      Authorization: `${this.getToken()}`,
     });
-    
-    return this.http
-    .put<newBook>(`${this.apiUrl}books/${bookId}`, updatedData, { headers })
-    .pipe(map((data: newBook) => data))
-    
-    
 
+    return this.http
+      .put<newBook>(`${this.apiUrl}books/${bookId}`, updatedData, { headers })
+      .pipe(map((data: newBook) => data));
   }
-  
+
   //pendientes de implementar
 
-  newUser(newUser: NewUser ){
+  newUser(newUser: NewUser) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'access_token': `${this.getToken()}`
-
+      access_token: `${this.getToken()}`,
     });
-      
-    return this.http.post<any>(`${this.apiUrl}/users/types`,newUser, {headers}).subscribe({
-        next: (response)=> {
+
+    return this.http
+      .post<any>(`${this.apiUrl}/users/types`, newUser, { headers })
+      .subscribe({
+        next: (response) => {
           response;
           this.message.successAlert();
           this.router.navigate(['/welcome']);
@@ -163,56 +151,46 @@ export class AuthService {
         error: (error) => {
           error;
           this.message.errorAlert(error.error.message);
-          
-        }
+        },
       });
-    
   }
 
-    isLoggedIn(): boolean {
-      const token = this.getToken();
-      if (token) {
-        // console.log(token);
-        const decodedToken: any = jwtDecode(token); // Decodifico el token
-        // console.log(decodedToken);
-        localStorage.setItem('currentUser', JSON.stringify(decodedToken));
-  
-        const expirationDate = new Date(decodedToken.exp * 1000); // Multiplico por 1000 para convertir a milisegundos
-        return expirationDate > new Date(); // true or false
-      }
-      return false;
+  isLoggedIn(): boolean {
+    const token = this.getToken();
+    if (token) {
+      // console.log(token);
+      const decodedToken: any = jwtDecode(token); // Decodifico el token
+      // console.log(decodedToken);
+      localStorage.setItem('currentUser', JSON.stringify(decodedToken));
+
+      const expirationDate = new Date(decodedToken.exp * 1000); // Multiplico por 1000 para convertir a milisegundos
+      return expirationDate > new Date(); // true or false
     }
-  
-    getUsers(): Observable<any>{
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'access_token': `${this.getToken()}`,
-    
-      });
-  
-      return this.http.get(`${this.apiUrl}/users`, { headers });
-    }
-  
-  
-    getUserByid(id:number): Observable<any>{
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'access_token': `${this.getToken()}`,
-    
-      });
-      return this.http.get(`${this.apiUrl}/users/${id}`, { headers });
-   
-    }
-  
-    GetTypesUsers(): Observable<any>{
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'access_token': `${this.getToken()}`,
-      });
-      return this.http.get(`${this.apiUrl}/users/types`, { headers });
-  
-    }
-  
-  
-  
+    return false;
+  }
+
+  getUsers(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      access_token: `${this.getToken()}`,
+    });
+
+    return this.http.get(`${this.apiUrl}/users`, { headers });
+  }
+
+  getUserByid(id: number): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      access_token: `${this.getToken()}`,
+    });
+    return this.http.get(`${this.apiUrl}/users/${id}`, { headers });
+  }
+
+  GetTypesUsers(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      access_token: `${this.getToken()}`,
+    });
+    return this.http.get(`${this.apiUrl}/users/types`, { headers });
+  }
 }
