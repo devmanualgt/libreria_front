fe48c4e7df73239c80e76945ebea54bb223b6e45
mconcoba <mconcoba20@gmail.com>
2024-09-20 18:01:42 -0600

    feat(change): url to apis

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://api-kars.vercel.app/api/v1',
-  //apiUrl: 'https://api-library-umg.vercel.app/api/',
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://api-library-umg.vercel.app/api/',
   SECRET_KEY_AUTH: 'devmanualgt@',
 };
