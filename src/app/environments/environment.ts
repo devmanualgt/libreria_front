28935897ec577320c0820dfb7d690ce7baea87ce
mconcoba <mconcoba20@gmail.com>
2024-10-17 16:02:27 -0600

    add: new url of apis

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    production: false,
-    // apiUrl: 'https://api-kars.vercel.app/api/v1',
-    apiUrl: 'https://api-library-umg.vercel.app/api/'
-
+  production: false,
+  // apiUrl: 'https://api-kars.vercel.app/api/v1',
+  //apiUrl: 'https://api-library-umg.vercel.app/api/'
+  apiUrl: 'http://api.library.devmanual.online/api/',
 };
-  
